8361103518b8e14e80afad11aadd71184cbe4523
Dennis Enderink <dennis@d-cre.nl>
2014-09-10 20:23:05 +0200

    Added IoC DIC, facades, asset and hook classes.

--- src/Core/Template.php
+++ src/Core/Template.php
@@ -2,8 +2,52 @@
 
 namespace Binocle\Core;
 
+use Hook;
+use Asset;
+
 class Template
 {
-// load wrapper if needed
-// load handle engine if needed
+	/**
+	 * Holds initial template
+	 * @var object|string
+	 */
+	protected $template;
+
+	/**
+	 * Constructor
+	 * @param object $container
+	 */
+	public function __construct($container)
+	{
+		$this->container = $container;
+
+		// load wrapper if needed
+		Hook::add('template_include', array($this, 'wrap'));
+
+		// add basic assets
+		Asset::addScript('jquery');
+		Asset::addStyle('app', get_template_directory_uri() . '/assets/css/app.css');
+	}
+
+	/**
+	 * Wraps initial template with ground layout
+	 * @param string $template
+	 * @return object|string
+	 */
+	public function wrap($template)
+	{
+		$this->template = Template\Loader::load($template);
+		$groundTemplate = Template\Loader::load('layout.ground');
+
+		return $groundTemplate;
+	}
+
+	/**
+	 * Loads initial template
+	 * @return void 
+	 */
+	public function loadTemplate()
+	{
+		include($this->template);
+	}
 }
--- src/Core/Theme.php
+++ src/Core/Theme.php
@@ -2,20 +2,50 @@
 
 namespace Binocle\Core;
 
-use Binocle\Core;
+use Binocle\Support\Container as Container;
 
 class Theme
 {
+	/**
+	 * Instance of IoC DIC
+	 * @var object
+	 */
+	private $container;
+
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
-		$hooks = new Hooks;
-		$template = new Template;
+		$this->container = Container::getInstance();
+		
+		// set dependencies
+		$this->container['hook'] = function($c) {
+			return new \Binocle\Core\Hook;
+		};
+
+		$this->container['template'] = function($c) {
+			return new \Binocle\Core\Template($c);
+		};
+
+		$this->container['asset'] = function($c) {
+			return new \Binocle\Core\Template\Asset;
+		};
+
+		class_alias('Binocle\Core\Facades\Asset', 'Asset');
+		class_alias('Binocle\Core\Facades\Hook', 'Hook');
 	}
 
-	// load config
-	
+	/**
+	 * Boot theme
+	 * @return void 
+	 */
 	public function boot()
 	{
-		echo 'booting';
+		// load template
+		$this->container['template'];
+
+		// clean up
+		
 	}
 }
